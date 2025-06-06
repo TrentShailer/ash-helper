8155630055d8e67de2eff8fd57ea2ad7a18b79fe
Trent Shailer <trent.shailer@gmail.com>
2025-06-07 11:54:05 +1200

    Update debug info

--- src/debug_utils.rs
+++ src/debug_utils.rs
@@ -1,4 +1,3 @@
-use alloc::borrow::Cow;
 use core::ffi::CStr;
 
 use ash::{ext, vk};
@@ -177,6 +176,164 @@ impl DebugUtils {
     }
 }
 
+/// Represents the data from a `vk::DebugUtilsMessengerCallbackDataEXT` with nice display.
+#[derive(Debug)]
+pub struct DebugMessage<'callback> {
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    id: i32,
+    vuid: &'callback CStr,
+    message: &'callback CStr,
+    queue_labels: &'callback [vk::DebugUtilsLabelEXT<'callback>],
+    command_buffer_labels: &'callback [vk::DebugUtilsLabelEXT<'callback>],
+    object_names: &'callback [vk::DebugUtilsObjectNameInfoEXT<'callback>],
+}
+
+impl<'callback> DebugMessage<'callback> {
+    /// Tries to convert a pointer to a `vk::DebugUtilsMessengerCallbackDataEXT` into self.
+    ///
+    /// # Safety
+    /// * `p_callback_data` **MUST** be safe to dereference if it is not a null pointer.
+    /// * All pointers in `DebugUtilsMessengerCallbackDataEXT` **MUST** be safe to dereference.
+    /// * All `*const c_char` elements **MUST** be null terminated and safe to dereference.
+    pub unsafe fn try_from(
+        p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT<'callback>,
+        message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    ) -> Option<Self> {
+        if p_callback_data.is_null() {
+            return None;
+        }
+
+        let callback_data = unsafe { *p_callback_data };
+
+        let id = callback_data.message_id_number;
+
+        let message = if !callback_data.p_message.is_null() {
+            unsafe { CStr::from_ptr(callback_data.p_message) }
+        } else {
+            c""
+        };
+
+        let vuid = if !callback_data.p_message_id_name.is_null() {
+            unsafe { CStr::from_ptr(callback_data.p_message_id_name) }
+        } else {
+            c""
+        };
+
+        let queue_labels = unsafe {
+            core::slice::from_raw_parts(
+                callback_data.p_queue_labels,
+                callback_data.queue_label_count as usize,
+            )
+        };
+
+        let command_buffer_labels = unsafe {
+            core::slice::from_raw_parts(
+                callback_data.p_cmd_buf_labels,
+                callback_data.cmd_buf_label_count as usize,
+            )
+        };
+
+        let object_names = unsafe {
+            core::slice::from_raw_parts(
+                callback_data.p_objects,
+                callback_data.object_count as usize,
+            )
+        };
+
+        Some(Self {
+            message_type,
+            id,
+            vuid,
+            message,
+            queue_labels,
+            command_buffer_labels,
+            object_names,
+        })
+    }
+}
+
+impl core::fmt::Display for DebugMessage<'_> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        // Shader debug `printf`
+        if self.id == 1985515673 {
+            return write!(f, "{}", self.message.to_string_lossy());
+        }
+
+        // `[Vulkan validation] VUID-vkCmdSetScissor-firstScissor-00593`
+        writeln!(
+            f,
+            "[Vulkan {}] {}",
+            format!("{:?}", self.message_type).to_lowercase(),
+            self.vuid.to_string_lossy()
+        )?;
+
+        // Message
+        writeln!(f, "    {}", self.message.to_string_lossy())?;
+
+        // Objects
+        if !self.object_names.is_empty() {
+            writeln!(f, "Objects:")?;
+
+            let mut seen_objects = Vec::new();
+            for object in self.object_names {
+                if seen_objects.contains(&object.object_handle) {
+                    continue;
+                }
+                seen_objects.push(object.object_handle);
+
+                let name = unsafe { object.object_name_as_c_str() }.unwrap_or(c"");
+
+                // `    [QUEUE] COMPUTE (0x5c11921d2d10)`
+                writeln!(
+                    f,
+                    "    [{:?}] {} (0x{:0x})",
+                    object.object_type,
+                    name.to_string_lossy(),
+                    object.object_handle
+                )?;
+            }
+        }
+
+        // Queue labels
+        if !self.queue_labels.is_empty() {
+            writeln!(f, "Queue labels:")?;
+
+            let mut previous_label = c"";
+            for queue in self.queue_labels.iter().rev() {
+                let label = unsafe { queue.label_name_as_c_str() }.unwrap_or(c"");
+
+                if previous_label == label {
+                    continue;
+                }
+                previous_label = label;
+
+                // `    HDR Scanner Read Result`
+                writeln!(f, "    {}", label.to_string_lossy())?;
+            }
+        }
+
+        // Command buffer labels
+        if !self.command_buffer_labels.is_empty() {
+            writeln!(f, "Command buffer labels:")?;
+
+            let mut previous_label = c"";
+            for command_buffer in self.command_buffer_labels.iter().rev() {
+                let label = unsafe { command_buffer.label_name_as_c_str() }.unwrap_or(c"");
+
+                if previous_label == label {
+                    continue;
+                }
+                previous_label = label;
+
+                // `    HdrScanner::scan`
+                writeln!(f, "    {}", label.to_string_lossy())?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
 /// Default messenger for Debug Utils.
 pub unsafe extern "system" fn vulkan_debug_callback(
     message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
@@ -184,48 +341,35 @@ pub unsafe extern "system" fn vulkan_debug_callback(
     p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT<'_>,
     _user_data: *mut core::ffi::c_void,
 ) -> vk::Bool32 {
-    let callback_data = unsafe { *p_callback_data };
-    let _message_id_number = callback_data.message_id_number;
-
-    let message_id_name = if callback_data.p_message_id_name.is_null() {
-        Cow::from("")
-    } else {
-        unsafe { CStr::from_ptr(callback_data.p_message_id_name).to_string_lossy() }
+    let Some(message) = (unsafe { DebugMessage::try_from(p_callback_data, message_type) }) else {
+        return vk::FALSE;
     };
 
-    let message = if callback_data.p_message.is_null() {
-        Cow::from("")
-    } else {
-        unsafe { CStr::from_ptr(callback_data.p_message).to_string_lossy() }
-    };
-
-    // Debug
-    if callback_data.message_id_number == 1985515673 {
-        debug!("{}", message);
+    // Shader debug `printf`
+    if message.id == 1985515673 {
+        debug!("{message}",);
         return vk::FALSE;
     }
 
-    let message = message.replace(" | ", "\n");
-
     match message_severity {
         vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE => {
-            debug!("[{message_type:?}] [{message_id_name}]\n{message}")
+            debug!("{message}")
         }
 
         vk::DebugUtilsMessageSeverityFlagsEXT::INFO => {
-            info!("[{message_type:?}] [{message_id_name}]\n{message}")
+            info!("{message}")
         }
 
         vk::DebugUtilsMessageSeverityFlagsEXT::WARNING => {
-            warn!("[{message_type:?}] [{message_id_name}]\n{message}")
+            warn!("{message}")
         }
 
         vk::DebugUtilsMessageSeverityFlagsEXT::ERROR => {
-            error!("[{message_type:?}] [{message_id_name}]\n{message}")
+            error!("{message}")
         }
 
         _ => {
-            info!("[{message_type:?}] [{message_id_name}]\n{message}")
+            info!("{message}")
         }
     };
 
